9d60b00fc3bef2bd596034e27da16ba15f699596
Nearsyh <nearsy.h@gmail.com>
2018-08-06 10:16:13 +0800

    update livy address

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ aws s3 cp measure/target/measure-0.2.0-incubating-SNAPSHOT.jar s3://jike-data-wa
 aws s3 cp measure/src/main/resources/env.json s3://jike-data-warehouse/configurations/griffin/env.json && \
 docker build -t "804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/griffin:$tag" --force-rm . && \
 docker tag "804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/griffin:$tag" "804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/griffin:latest" && \
+aws ecr get-login --no-include-email | bash && \
 docker push "804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/griffin:$tag" && \
 docker push "804775010343.dkr.ecr.cn-north-1.amazonaws.com.cn/griffin:latest"
