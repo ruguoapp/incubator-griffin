75c05bc2cc45a665f0662c4f7cccd78c25e36ffc
Nearsyh <nearsy.h@gmail.com>
2018-08-10 11:24:15 +0800

    Implement match ratio and add test cases

--- measure/src/main/scala/com/okjike/griffin/dsl/functions/Count.scala
+++ measure/src/main/scala/com/okjike/griffin/dsl/functions/Count.scala
@@ -18,6 +18,38 @@ object Count {
     }
   }
 
+  def match_ratio(src_table: String, dst_table: String, cols: Array[(String, String)],
+                  name: String = "match_ratio",
+                  src_conditions: String = "", dst_conditions: String = ""): BaseFunction = {
+    sqlContext => {
+      var join_count_query = getJoinCountQuery(src_table, dst_table, cols)
+      var conditions = if (src_conditions == null) "" else src_conditions
+      if (dst_conditions != null && !dst_conditions.isEmpty) {
+        conditions = if (conditions.isEmpty) dst_conditions else conditions + " and " + dst_conditions
+      }
+      if (!conditions.isEmpty) {
+        join_count_query = join_count_query + " where " + conditions
+      }
+      sqlContext.sql(join_count_query).show(10)
+      val match_count = sqlContext.sql(join_count_query).collect().head.getLong(0)
+
+      var count_query = f"select count(*) from $src_table"
+      if (src_conditions != null && !src_conditions.isEmpty) {
+        count_query = count_query + " where " + src_conditions
+      }
+      val total_count = sqlContext.sql(count_query).collect().head.getLong(0)
+      val match_ratio = 1.0 * match_count / total_count
+      sqlContext.createDataFrame(
+        ImmutableList.of(Row(match_ratio)),
+        StructType(Array(StructField(name, DoubleType))))
+    }
+  }
+
+  private[Count] def getJoinCountQuery(left: String, right: String, on: Array[(String, String)]): String = {
+    val onClauses = on.map(tuple2 => f"""${tuple2._1} = ${tuple2._2}""").mkString(" and ")
+    f"""select count(*) from $left inner join $right on $onClauses"""
+  }
+
   private[Count] def getCountQuery(table: String, offset: Int = 1, alias: String = "c"): String = {
     f"""select count(*) as c from $table where dt=date_sub(current_date(), $offset)"""
   }
--- measure/src/test/scala/com/okjike/griffin/dsl/JikeDslTest.scala
+++ measure/src/test/scala/com/okjike/griffin/dsl/JikeDslTest.scala
@@ -1,5 +1,8 @@
 package com.okjike.griffin.dsl
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import com.okjike.griffin.dsl.evaluator.Eval
 import com.okjike.griffin.dsl.functions.Type.BaseFunction
@@ -14,13 +17,33 @@ class JikeDslTest extends FlatSpec with Matchers with DataFrameSuiteBase {
     val schema = StructType(Array(
       StructField("dt", StringType)
     ))
-    val rows = Seq(Row("2018-08-06", "2018-08-05", "2018-08-06"))
+
+    val today = DateTimeFormatter.ISO_LOCAL_DATE.format(LocalDate.now())
+    val yesterday = DateTimeFormatter.ISO_LOCAL_DATE.format(LocalDate.now().minusDays(1))
+    val rows = Seq(Row(today), Row(yesterday), Row(today))
     val rowRdd = sqlContext.sparkContext.parallelize(rows)
     sqlContext.createDataFrame(rowRdd, schema).createOrReplaceTempView("dates")
 
-    val df = Eval.apply[BaseFunction]("import com.okjike.griffin.dsl.functions._\nCount.diff_ratio(\"dates\", \"difference\")")
+    val df = Eval.apply[BaseFunction]("import com.okjike.griffin.dsl.functions._\nCount.diff_ratio(\"dates\", \"difference\", 0)")
       .apply(sqlContext)
-    df.show(10)
+    df.collect().head.getDouble(0) should === (1.0)
+  }
+
+  "count match_ratio" should "succeed" in {
+    val schema = StructType(Array(
+      StructField("id", StringType)
+    ))
+    val left = Seq(Row("1"), Row("2"), Row("3"))
+    val leftRdd = sqlContext.sparkContext.parallelize(left)
+    sqlContext.createDataFrame(leftRdd, schema).createOrReplaceTempView("left")
+
+    val right = Seq(Row("1"), Row("2"))
+    val rightRdd = sqlContext.sparkContext.parallelize(right)
+    sqlContext.createDataFrame(rightRdd, schema).createOrReplaceTempView("right")
+
+    val df = Eval.apply[BaseFunction]("import com.okjike.griffin.dsl.functions._\nCount.match_ratio(\"left\", \"right\", Array((\"left.id\", \"right.id\")))")
+      .apply(sqlContext)
+    df.collect().head.getDouble(0) should === (2.0 / 3)
   }
 
   "jike dsl" should "be parsed successfully" in {
